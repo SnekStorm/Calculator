29dad6e10a0cbc3bfff747311f07ad61ffb5b503
Viktor <viktor.aksson@gmail.com>
2023-01-31 23:03:39 +0100

    Created main file

---
+++ calc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string input;
+    
+    
+
+    cout << "hej" << endl;;
+    cin >> input;
+
+    for (int i = 0; i < size(input); i++)
+    {
+        cout << i << " " << input[i] << endl;;
+
+        if (input[i] == "+")
+        {
+            /* code */
+        }
+        
+    }
+    
+
+
+
+
+    cout << input << " LOL " << endl;
+
+    return 0;
+}
