20b8f5c79b3fafa00defad3f2bf89511fb620860
Viktor <viktor.aksson@gmail.com>
2023-02-01 22:46:23 +0100

    Added hpp file and Struct for memory

--- calc.cpp
+++ calc.cpp
@@ -1,59 +1,47 @@
-#include <iostream>
+
+#include "storage.hpp"
 using namespace std;
 
-string indexNumbers(int start_index, int end_index, int index, string input)
+
+
+
+void indexNumbers()
 {
-	string temp;
-	for (int j = 0; j < (end_index-start_index); j++)
+string temp;
+	for (int j = 0; j < (memory.end_index-memory.start_index); j++)
 	{    
-		temp[index] += input[start_index+j];
+		 memory.numberArray[memory.index] += memory.input[memory.start_index+j];
 	}
-	return temp;
 }
 
 int main()
 {
-    string input;
-    string temp;
+    cout << "Enter formula: ";
+    cin >> memory.input;
+	cout << endl;
 
-    int start_index = 0;
-    int end_index = 0;
-    int index = 0;
-
-    string numberArray[10];
-    
-
-    cout << "hej" << endl;
-    cin >> input;
-
-    for (int i = 0; i < size(input)+1; i++)
+    for (int i = 0; i < size(memory.input)+1; i++)
     {
-        end_index = i;  
-        if (input[i] == '+')
+        memory.end_index = i;  
+        if (memory.input[i] == '+')
         {
-            indexNumbers(start_index, end_index, index, input);
+            indexNumbers();
             //cout << "START: "<< start_index << "  END: " << end_index << endl;
-            start_index = end_index+1;
-            index++;
-            
+            memory.start_index = memory.end_index+1;
+            memory.index++;
         }
         
     }
     //cout << "START: "<< start_index << "  END: " << end_index << endl;
     //cout << "both: "<< (end_index-start_index) << "  index: " << index << endl;
-    indexNumbers(start_index, end_index, index, input);
-	
+    indexNumbers();
 
 
+    cout << "Input: " << memory.input << endl;
 
-
-
-
-
-    cout << "Input: " << input << endl;
-    for (int i = 0; i < index+1; i++)
+    for (int i = 0; i < memory.index+1; i++)
     {
-        cout << "Output array: "<< numberArray[i] << endl;
+        cout << "Output array: "<< memory.numberArray[i] << endl;
     }
 
     return 0;
---
+++ storage.hpp
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+
+#include <iostream>
+using namespace std;
+
+
+   struct 
+   {  
+      string input;
+      string numberArray[10];
+      int start_index = 0;
+      int end_index = 0;   
+      int index = 0;
+
+   }memory;
+   
+
+#endif
