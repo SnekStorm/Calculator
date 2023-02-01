a762867b51464754641b5baedc304bf09721658b
Viktor <viktor.aksson@gmail.com>
2023-02-01 22:07:43 +0100

    Input and Array splitter (Still String)

--- calc.cpp
+++ calc.cpp
@@ -1,31 +1,60 @@
 #include <iostream>
 using namespace std;
 
+string indexNumbers(int start_index, int end_index, int index, string input)
+{
+	string temp;
+	for (int j = 0; j < (end_index-start_index); j++)
+	{    
+		temp[index] += input[start_index+j];
+	}
+	return temp;
+}
+
 int main()
 {
     string input;
-    
+    string temp;
+
+    int start_index = 0;
+    int end_index = 0;
+    int index = 0;
+
+    string numberArray[10];
     
 
-    cout << "hej" << endl;;
+    cout << "hej" << endl;
     cin >> input;
 
-    for (int i = 0; i < size(input); i++)
+    for (int i = 0; i < size(input)+1; i++)
     {
-        cout << i << " " << input[i] << endl;;
-
-        if (input[i] == "+")
+        end_index = i;  
+        if (input[i] == '+')
         {
-            /* code */
+            indexNumbers(start_index, end_index, index, input);
+            //cout << "START: "<< start_index << "  END: " << end_index << endl;
+            start_index = end_index+1;
+            index++;
+            
         }
         
     }
-    
+    //cout << "START: "<< start_index << "  END: " << end_index << endl;
+    //cout << "both: "<< (end_index-start_index) << "  index: " << index << endl;
+    indexNumbers(start_index, end_index, index, input);
+	
 
 
 
 
-    cout << input << " LOL " << endl;
+
+
+
+    cout << "Input: " << input << endl;
+    for (int i = 0; i < index+1; i++)
+    {
+        cout << "Output array: "<< numberArray[i] << endl;
+    }
 
     return 0;
 }
